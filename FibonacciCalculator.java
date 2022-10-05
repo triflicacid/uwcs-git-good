f6d9b4577494bbb1a89f3fd7779384dbf413e3c0
TriflicAcid <rubensaunders@hotmail.co.uk>
2022-10-05 15:40:21 +0100

    Made calculator more efficient

--- FibonacciCalculator.java
+++ FibonacciCalculator.java
@@ -1,9 +1,13 @@
 public class FibonacciCalculator {
     public int calculate(int n) {
-        if (n <= 0 || n == 1)
-            return n;
-        else
-            return calculate(n - 1) + calculate(n - 2);
+        return fib(n, 1, 0);
+    }
+
+    private int fib(int term, int val, int prev) {
+        if (term == 0)
+            return prev;
+        return fib(term - 1, val + prev, val);
+
     }
 }
 
